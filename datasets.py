99633317175620904652cb3c32204a79891bae85
huu5 <165475036+huu5@users.noreply.github.com>
2024-06-22 21:44:05 +0800

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import torch
+import dgl
+from dgl.data.fraud import FraudAmazonDataset, FraudYelpDataset
+from sklearn.model_selection import train_test_split
+
+DATA_NAMES = ['yelp', 'amazon', 'amazon_new']
+
+
+def load_data(data_name, multi_relation, raw_dir='./data', seed=42, train_size=0.1, test_size=0.67):
+	
+	assert data_name in DATA_NAMES
+
+	if data_name == 'yelp':
+		graph = FraudYelpDataset(raw_dir).graph
+	elif data_name.startswith('amazon'):
+		graph = FraudAmazonDataset(raw_dir).graph
+		if data_name == 'amazon_new':
+			features = graph.ndata['feature'].numpy()
+			mask_dup = torch.BoolTensor(pd.DataFrame(features).duplicated(keep=False).values)
+			graph = graph.subgraph(~mask_dup)
+	
+	# Rename ndata & Remove redundant data
+	# graph.ndata['x'] = graph.ndata['feature']
+	# graph.ndata['y'] = graph.ndata['label']
+	# del graph.ndata['feature'], graph.ndata['label']
+	# del graph.ndata['train_mask'], graph.ndata['val_mask'], graph.ndata['test_mask']
+	if not multi_relation:
+		graph = dgl.to_homogeneous(graph, ndata=['feature', 'label'], store_type=False)
+		# graph.ndata['x'] = graph.ndata['feature']
+		# graph.ndata['y'] = graph.ndata['label']
+		# del graph.ndata['_ID'], graph.edata['_ID']
+
+	for etype in graph.etypes:
+		graph = dgl.add_self_loop(graph, etype=etype)
+	labels = graph.ndata["label"]
+	# [STEP-3] Split the train/valid/test dataset with stratified sampling.
+	# if data_name.startswith('amazon'):
+		# idx_unlabeled = 2013 if data_name == 'amazon_new' else 3305
+		# As 0-3304 are unlabeled nodes, they are excepted for the train/valid/test process.
+		# index = list(range(idx_unlabeled, len(labels)))
+		# idx_train, idx_rest, y_train, y_rest = train_test_split(index, labels[idx_unlabeled:],
+		# 														stratify=labels[idx_unlabeled:],
+		# 														train_size=train_size,
+		# 														random_state=seed, shuffle=True)
+		# idx_valid, idx_test, y_valid, y_test = train_test_split(idx_rest, y_rest, stratify=y_rest,
+		# 														test_size=test_size,
+		# 														random_state=seed, shuffle=True)
+	# else:
+	# 	index = list(range(len(labels)))
+		# idx_train, idx_rest, y_train, y_rest = train_test_split(index, labels, stratify=labels,
+		# 														train_size=train_size,
+		# 														random_state=seed, shuffle=True)
+		# idx_valid, idx_test, y_valid, y_test = train_test_split(idx_rest, y_rest, stratify=y_rest,
+		# 														test_size=test_size,
+		# 														random_state=seed, shuffle=True)
+
+	return graph
+
+
+if __name__ == '__main__':
+	g = load_data('yelp', multi_relation=False)
+	print(g.ndata.items())
+	node = g.ndata['_ID']
+	edge = g.edata['_ID']
+	print(g.ndata['label'].unique())
+
---
+++ model_custom.py
@@ -0,0 +1,176 @@
+from torch import nn
+from layers_custom import *
+from torch.nn.parameter import Parameter
+from utils import sparse_mx_to_torch_sparse_tensor
+import scipy.sparse as sp
+
+
+device = torch.device("cuda:0")
+
+
+class SimPGCN(nn.Module):
+    """
+       The model for the single kind of deepgcn blocks.
+
+       The model architecture likes:
+       inputlayer(nfeat)--block(nbaselayer, nhid)--...--outputlayer(nclass)--softmax(nclass)
+                           |------  nhidlayer  ----|
+       The total layer is nhidlayer*nbaselayer + 2.
+       All options are configurable.
+    """
+
+    def __init__(self,
+                 nfeat,
+                 nhid,
+                 nclass,
+                 nhidlayer,
+                 dropout,
+                 baseblock="mutigcn",
+                 inputlayer="gcn",
+                 outputlayer="gcn",
+                 nbaselayer=0,
+                 activation=lambda x: x,
+                 withbn=True,
+                 withloop=True,
+                 aggrmethod="add",
+                 mixmode=False, **kwargs):
+        """
+        Initial function.
+        :param nfeat: the input feature dimension.
+        :param nhid:  the hidden feature dimension.
+        :param nclass: the output feature dimension.
+        :param nhidlayer: the number of hidden blocks.
+        :param dropout:  the dropout ratio.
+        :param baseblock: the baseblock type, can be "mutigcn", "resgcn", "densegcn" and "inceptiongcn".
+        :param inputlayer: the input layer type, can be "gcn", "dense", "none".
+        :param outputlayer: the input layer type, can be "gcn", "dense".
+        :param nbaselayer: the number of layers in one hidden block.
+        :param activation: the activation function, default is ReLu.
+        :param withbn: using batch normalization in graph convolution.
+        :param withloop: using self feature modeling in graph convolution.
+        :param aggrmethod: the aggregation function for baseblock, can be "concat" and "add". For "resgcn", the default
+                           is "add", for others the default is "concat".
+        :param mixmode: enable cpu-gpu mix mode. If true, put the inputlayer to cpu.
+        """
+        super(SimPGCN, self).__init__()
+        # self.mixmode = mixmode
+        self.dropout = dropout
+
+        print('=== Number of Total Layers is %s ===' % (nhidlayer * nbaselayer + 2))
+        self.BASEBLOCK = MultiLayerGCNBlock
+
+        # input gc
+        self.ingc = GraphConvolutionBS(nfeat, nhid, activation, withbn, withloop)
+        baseblockinput = nhid
+
+        # hidden layer
+        self.midlayer = nn.ModuleList()
+        # Dense is not supported now.
+        # for i in xrange(nhidlayer):
+        for i in range(nhidlayer):
+            gcb = self.BASEBLOCK(in_features=baseblockinput,
+                                 out_features=nhid,
+                                 nbaselayer=nbaselayer,
+                                 withbn=withbn,
+                                 withloop=withloop,
+                                 activation=activation,
+                                 dropout=dropout,
+                                 dense=False,
+                                 aggrmethod=aggrmethod)
+            self.midlayer.append(gcb)
+            baseblockinput = gcb.get_outdim()
+        # output gc
+        outactivation = lambda x: x  # we donot need nonlinear activation here.
+        self.outgc = GraphConvolutionBS(baseblockinput, nclass, outactivation, withbn, withloop)
+
+        self.reset_parameters()
+
+        self.scores = nn.ParameterList()  # 平衡原始图和特征图效果的分数向量  （n, 1）
+        self.scores.append(Parameter(torch.FloatTensor(nfeat, 1)))
+        for i in range(nhidlayer):
+            self.scores.append(Parameter(torch.FloatTensor(nhid, 1)))
+
+        for s in self.scores:
+            # s.data.fill_(0)
+            stdv = 1. / math.sqrt(s.size(1))
+            s.data.uniform_(-stdv, stdv)
+            # glorot(s)
+            # zeros(self.bias)
+
+        self.bias = nn.ParameterList()
+        self.bias.append(Parameter(torch.FloatTensor(1)))
+        for i in range(nhidlayer):
+            self.bias.append(Parameter(torch.FloatTensor(1)))
+        for b in self.bias:
+            b.data.fill_(
+                kwargs['bias_init'])  # fill in b with postive value to make score s closer to 1 at the beginning
+
+        # self.D_k = Parameter(torch.FloatTensor(kwargs['nnodes'], 1))
+        self.D_k = nn.ParameterList()  # self-loop自适应系数
+        self.D_k.append(Parameter(torch.FloatTensor(nfeat, 1)))
+        for i in range(nhidlayer):
+            self.D_k.append(Parameter(torch.FloatTensor(nhid, 1)))
+        for Dk in self.D_k:
+            stdv = 1. / math.sqrt(Dk.size(1))
+            Dk.data.uniform_(-stdv, stdv)
+            # glorot(Dk)
+        self.identity = sparse_mx_to_torch_sparse_tensor(sp.eye(kwargs['nnodes'])).to(device)
+
+        self.D_bias = nn.ParameterList()
+        self.D_bias.append(Parameter(torch.FloatTensor(1)))
+        for i in range(nhidlayer):
+            self.D_bias.append(Parameter(torch.FloatTensor(1)))
+        for b in self.D_bias:
+            b.data.fill_(0)  # fill in b with postive value to make score s closer to 1 at the beginning
+
+        self.gamma = kwargs['gamma']
+
+    def reset_parameters(self):
+        pass
+
+    def forward(self, fea, adj, adj_knn):
+        x, _ = self.myforward(fea, adj, adj_knn)
+        return x
+
+    def myforward(self, fea, adj, adj_knn, layer=1.5):
+        '''output embedding and log_softmax'''
+        gamma = self.gamma  # Eq(11)中的超参数
+
+        use_Dk = True
+        s_i = torch.sigmoid(fea @ self.scores[0] + self.bias[0])  # Eq(10)
+
+        if use_Dk:
+            Dk_i = (fea @ self.D_k[0] + self.D_bias[0])  # Eq(12)
+            x = (s_i * self.ingc(fea, adj) + (1-s_i) * self.ingc(fea, adj_knn)) + (gamma) * Dk_i * self.ingc(fea, self.identity)    # Eq(9) + Eq(11)
+        else:
+            x = s_i * self.ingc(fea, adj) + (1-s_i) * self.ingc(fea, adj_knn)  # Eq(9)
+
+        if layer ==1:
+            embedding = x.clone()
+
+        x = F.dropout(x, self.dropout, training=self.training)
+        if layer == 1.5:
+            embedding = x.clone()
+
+        # mid block connections
+        # for i in xrange(len(self.midlayer)):
+        for i in range(len(self.midlayer)):
+            midgc = self.midlayer[i]
+            x = midgc(x, adj)
+        if layer == -2:
+            embedding = x.clone()
+
+        # output, no relu and dropput here.
+        s_o = torch.sigmoid(x @ self.scores[-1] + self.bias[-1])
+        if use_Dk:
+            Dk_o = (x @ self.D_k[-1] + self.D_bias[-1])
+            x = (s_o * self.outgc(x, adj) + (1-s_o) * self.outgc(x, adj_knn)) + (gamma) * Dk_o * self.outgc(x, self.identity)
+        else:
+            x = s_o * self.outgc(x, adj) + (1-s_o) * self.outgc(x, adj_knn)
+
+        if layer == -1:
+            embedding = x.clone()
+        x = F.log_softmax(x, dim=1)
+
+        self.ss = torch.cat((s_i.view(1,-1), s_o.view(1,-1), gamma*Dk_i.view(1,-1), gamma*Dk_o.view(1,-1)), dim=0)
+        return x, embedding
